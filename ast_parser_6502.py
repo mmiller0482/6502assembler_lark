daa5b9b97de9cb9e57e9e0e99fe5068a3a00e44b
matthewmiller <mmiller0482@gmail.com>
2025-08-16 09:22:59 -0400

    Wip

--- ast_parser_6502.py
+++ ast_parser_6502.py
@@ -2,7 +2,7 @@ from lark import Lark, Tree
 
 
 class AstParser6502:
-    def __init__(self, grammar: str, start_tag: str = "value"):
+    def __init__(self, grammar: str, start_tag: str = "start"):
         self._parser = Lark(grammar, start=start_tag)
 
     def parse(self, text: str):
--- main.py
+++ main.py
@@ -2,8 +2,14 @@ from ast_parser_6502 import AstParser6502
 from ast_parser_6502_factory import AstParser6502Factory
 
 if __name__ == "__main__":
-    str_to_parse = '{"key": ["item0", "item1", 3.14]}'
+    str_to_parse = """
+A
+A_LABEL0:
+_LABEL1:
+LABEL2:
+$AF
+    """
 
     parser: AstParser6502 = AstParser6502Factory.build()
     ast = parser.parse(str_to_parse)
-    print(ast)
+    print(ast.pretty())
