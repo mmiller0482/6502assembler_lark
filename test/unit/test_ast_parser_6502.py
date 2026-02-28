7f015fac5fdb865b60b782f4ff7c2ed0860c1be3
matthewmiller <mmiller0482@gmail.com>
2026-02-28 16:50:06 +0100

    added some new tests

--- test/unit/test_ast_parser_6502.py
+++ test/unit/test_ast_parser_6502.py
@@ -1,23 +1,56 @@
 import pytest
 
-from assembler.ast_parse import AstParser6502Factory
+from assembler.ast_parse.ast_parser_6502 import AstParser6502
+from assembler.ast_parse.ast_parser_6502_factory import AstParser6502Factory
 
 
 @pytest.fixture
 def gen_parser():
-    def gen_parser_at_start(start_tag="start"):
+    def gen_parser_fixture(start_tag="start"):
         return AstParser6502Factory.build(start_tag=start_tag)
 
-    return gen_parser_at_start
+    return gen_parser_fixture
 
 
 @pytest.mark.parametrize(
     "source, expected",
     [
         ("42", 42),
+        ("$100", 256),
+        ("%1011", 11)
     ],
 )
 def test_parse_number(gen_parser, source: str, expected: int):
-    parser = gen_parser("numeric_val")
+    parser : AstParser6502 = gen_parser("numeric_val")
     ast = parser.parse(source)
     # never reach assert because the grammar does not allow for simply returning a terminal.
+    assert ast == expected
+
+def test_comment(gen_parser):
+    # Comment should do nothing :)
+    parser : AstParser6502 = gen_parser("comment")
+    ast = parser.parse(";comment")
+    assert ast is None
+
+@pytest.mark.parametrize(
+    "source, expected",
+    [
+        ("#42", 42),
+        ("#$100", 256),
+        ("#%1011", 11)
+    ],
+)
+def test_immediate(gen_parser, source: str, expected: int):
+    # Here we need to test that immediate hashing is performed correctly and
+    # splits the hash string into two parts
+    parser : AstParser6502 = gen_parser("immediate")
+    ast = parser.parse(source)
+
+    # TODO: need to comprehend from ast_builder code why immediate gets
+    # generated the way it does
+    assert isinstance(ast, dict)
+    assert "mode" in ast
+    assert ast["mode"] == "imm"
+
+    assert "expr" in ast
+    assert ast["expr"] == expected
