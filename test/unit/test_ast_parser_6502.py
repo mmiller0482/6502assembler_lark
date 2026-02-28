9ee3c611b8fbbdd462151e0deeb6ea945bbf02b9
matthewmiller <mmiller0482@gmail.com>
2026-02-28 18:08:23 +0100

    black formatting

--- test/unit/test_ast_parser_6502.py
+++ test/unit/test_ast_parser_6502.py
@@ -12,41 +12,33 @@ def gen_parser():
 
     return gen_parser_fixture
 
-class TestAstParser6502:
 
+class TestAstParser6502:
 
     @pytest.mark.parametrize(
         "source, expected",
-        [
-            ("42", 42),
-            ("$100", 256),
-            ("%1011", 11)
-        ],
+        [("42", 42), ("$100", 256), ("%1011", 11)],
     )
     def test_parse_number(self, gen_parser, source: str, expected: int):
-        parser : AstParser6502 = gen_parser("numeric_val")
+        parser: AstParser6502 = gen_parser("numeric_val")
         ast = parser.parse(source)
         # never reach assert because the grammar does not allow for simply returning a terminal.
         assert ast == expected
 
     def test_comment(self, gen_parser):
         # Comment should do nothing :)
-        parser : AstParser6502 = gen_parser("comment")
+        parser: AstParser6502 = gen_parser("comment")
         ast = parser.parse(";comment")
         assert ast is None
 
     @pytest.mark.parametrize(
         "source, expected",
-        [
-            ("#42", 42),
-            ("#$100", 256),
-            ("#%1011", 11)
-        ],
+        [("#42", 42), ("#$100", 256), ("#%1011", 11)],
     )
     def test_immediate(self, gen_parser, source: str, expected: int):
         # Here we need to test that immediate hashing is performed correctly and
         # splits the hash string into two parts
-        parser : AstParser6502 = gen_parser("immediate")
+        parser: AstParser6502 = gen_parser("immediate")
         ast = parser.parse(source)
 
         # TODO: need to comprehend from ast_builder code why immediate gets
